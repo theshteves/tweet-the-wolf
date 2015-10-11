39c5c49a5817b3fa4527d442c8e6383233036a63
theshteves <kneisers@msu.edu>
2015-10-11 01:19:57 -0400

    Bot catches all common errors and continues

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from wolf import Wolf
 import json
 import requests
 import sys
@@ -9,46 +8,42 @@ import time
 import tweepy
 import wolframalpha
 from keys import *
+from tweepy.error import TweepError
+from wolf import Wolf
+
 
 # authenticate with twitter & wolfram alpha
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+
 # grab all tweets with "#tweetthewolf"
 cursor = tweepy.Cursor(api.search, q="tweetthewolf")
 for status in cursor.items():
 
-    #if bot has not replied:
-    #print("\n\n\nTWEEPY STATUS:\n" + json.dumps(status._json, indent=4, sort_keys=True))
-    #try:
-    print("status.text: " + str(status.text))
+    #print(json.dumps(status._json, indent=2, separators=(',',':'), sort_keys=True))
 
-    wolf = Wolf(WOLFRAM_KEY, status.text)
-    print(wolf.result())
-    #print(json.dumps(wolf.request()["queryresult"]["pods"][1]["subpods"][0]["plaintext"], indent=2, separators=(',',':'), sort_keys=True))
-    answer = str(wolf.request()["queryresult"]["pods"][1]["subpods"][0]["plaintext"])
+    try:
+        print("status.text: " + str(status.text))
 
-    api.update_status(status=answer, in_reply_to_status_id = status.id)
-    #except TweepError:
-     #   print("\n\nlel.\n\n")
+        wolf = Wolf(WOLFRAM_KEY, status.text)
+        print(wolf.result())
+        # For debugging:
+        # print(json.dumps(wolf.request()["queryresult"]["pods"][1]["subpods"][0]["plaintext"], indent=2, separators=(',',':'), sort_keys=True))
+        answer = str(wolf.request()["queryresult"]["pods"][1]["subpods"][0]["plaintext"])
+        answer = "@" + status._json[0]["screen_name"] + " " + answer
 
-    '''
-    res = client.query("12*12")#str(status.text))
-    res = list(res)
-    for n in res:
-        print("\nbruh: " + str(n.text))
-        #print("\n\n\nWOLFRAM|ALPHA RESULT:\n" + str(res.results[0].text))
-    '''
+    except TweepError as err:
+        print("TweepError: " + str(err))
+        continue
 
-    '''
-    outfile = open('request.txt','w')
-    outfile.write(str(status.id))
-    outfile.close()
+    except KeyError as err:
+        print("KeyError: " + str(err))
+        continue
 
-    infile = open('response.txt','r')
-    f = infile.readlines()
-    infile.close()
+    else:
+        api.update_status(status=answer, in_reply_to_status_id = status.id)
 
-    #print("file:\n" + str(f[0]))
-    '''
+    finally:
+        print("\n")
